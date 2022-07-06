16a0c94f8d42b6e675216d84bdac6e118c5535c7
Evgen_gaydar <e.gaydarenko@icloud.com>
2022-07-06 23:29:44 +0300

    8th homework done

--- MyFirstProjectC/Program.cs
+++ MyFirstProjectC/Program.cs
@@ -58,6 +58,49 @@
 //else res = a - b;
 //Console.WriteLine(res);
 // 3
+//Console.WriteLine("Enter a number");
+//double a = Convert.ToDouble(Console.ReadLine());
+//if (a % 2 == 0) Console.WriteLine("Четное");
+
+
+// 8th lesson
+// 1
+//Console.WriteLine("Enter a number");
+//double a = Convert.ToDouble(Console.ReadLine());
+//if (a % 2 == 0) Console.WriteLine("Четное");
+//else Console.WriteLine("Нечетное");
+// 2
+//Console.WriteLine("A =");
+//double a = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("B =");
+//double b = Convert.ToDouble(Console.ReadLine());
+//if (a > b) Console.WriteLine("A > B на " + Convert.ToString(a - b));
+//else if (a < b) Console.WriteLine("B > A на " + Convert.ToString(b - a));
+//else Console.WriteLine("A и В равны");
+// 3
+//Console.WriteLine("A =");
+//double a = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("B =");
+//double b = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("C =");
+//double c = Convert.ToDouble(Console.ReadLine());
+//if (a > b)
+//    if (a > c)
+//        if (b > c) Console.WriteLine(c + " " + b + " " + a);
+//        else Console.WriteLine(b + " " + c + " " + a);
+//    else Console.WriteLine(b + " " + a + " " + c);
+//else if (b > c)
+//    if (a > c) Console.WriteLine(c + " " + a + " " + b);
+//    else Console.WriteLine(a + " " + c + " " + b);
+//else Console.WriteLine(a + " " + b + " " + c);
+// 4
 Console.WriteLine("A =");
 double a = Convert.ToDouble(Console.ReadLine());
-if (a % 2 == 0) Console.WriteLine("Четное");
+Console.WriteLine("B =");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("C =");
+double c = Convert.ToDouble(Console.ReadLine());
+string x1 = Convert.ToString((-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a));
+string x2 = Convert.ToString((-b - Math.Sqrt(b * b - 4 * a * c)) / (2 * a));
+Console.WriteLine("x1 = " + x1);
+Console.WriteLine("x2 = " + x2);
