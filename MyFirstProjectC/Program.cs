ec28b513cab13894266f61eb9dd9d901c45922f8
Evgen_gaydar <e.gaydarenko@icloud.com>
2022-07-06 21:42:53 +0300

    Add project files.

---
+++ MyFirstProjectC/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Как Вас зовут?");
+string firstName = Console.ReadLine();
+string hello = "Привет, " + firstName + "!!!";
+Console.WriteLine(hello);
+
+Console.WriteLine("Какая у Вас фамилия?");
+string lastName = Console.ReadLine();
+Console.WriteLine("А какое у Вас отчество?");
+string middleName = Console.ReadLine();
+string fullName = lastName + " " + firstName + " " + middleName;
+Console.WriteLine(fullName);
