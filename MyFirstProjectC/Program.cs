e7b73c262866ca35501a9ed0fbf28da056b054b3
Evgen_gaydar <e.gaydarenko@icloud.com>
2022-07-06 22:26:30 +0300

    6th homework's done

--- MyFirstProjectC/Program.cs
+++ MyFirstProjectC/Program.cs
@@ -1,11 +1,41 @@
-﻿Console.WriteLine("Как Вас зовут?");
-string firstName = Console.ReadLine();
-string hello = "Привет, " + firstName + "!!!";
-Console.WriteLine(hello);
+﻿// 5 lesson
+//Console.WriteLine("Как Вас зовут?");
+//string firstName = Console.ReadLine();
+//string hello = "Привет, " + firstName + "!!!";
+//Console.WriteLine(hello);
 
-Console.WriteLine("Какая у Вас фамилия?");
-string lastName = Console.ReadLine();
-Console.WriteLine("А какое у Вас отчество?");
-string middleName = Console.ReadLine();
-string fullName = lastName + " " + firstName + " " + middleName;
-Console.WriteLine(fullName);
+//Console.WriteLine("Какая у Вас фамилия?");
+//string lastName = Console.ReadLine();
+//Console.WriteLine("А какое у Вас отчество?");
+//string middleName = Console.ReadLine();
+//string fullName = lastName + " " + firstName + " " + middleName;
+//Console.WriteLine(fullName);
+
+
+// 6th lesson
+// 1
+//Console.WriteLine("A =");
+//double a = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("B =");
+//double b = Convert.ToDouble(Console.ReadLine());
+//string res = "A * B =" + Convert.ToString(a * b);
+//Console.WriteLine(res);
+//res = "A % B =" + Convert.ToString(b % a);
+//Console.WriteLine(res);
+// 2
+//Console.WriteLine("A =");
+//double a = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("B =");
+//double b = Convert.ToDouble(Console.ReadLine());
+//double c = a;
+//a = b;
+//b = c;
+// 3
+Console.WriteLine("A =");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("B =");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("C =");
+double c = Convert.ToDouble(Console.ReadLine());
+string res = Convert.ToString((c - b) / a);
+Console.WriteLine("X = " + res);
