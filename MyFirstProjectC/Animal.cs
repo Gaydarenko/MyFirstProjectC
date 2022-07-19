bca985200acda50017bf700b41149d911d32d62f
Evgen_gaydar <e.gaydarenko@icloud.com>
2022-07-19 23:04:52 +0300

    22th homework

---
+++ MyFirstProjectC/Animal.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProjectC
+{
+    public class Animal
+    {
+        public string Kind { 
+            get
+            {
+                return $"Вид - {_kind}";
+            }
+            set
+            {
+                _kind = value;
+            }
+        }
+        private string _kind;
+        public string RequiredBiom { 
+            get
+            {
+                return $"Необходимый биом - {_requiredbiom}";
+            }
+            set
+            {
+                _requiredbiom = value;
+            }
+        }
+        private string _requiredbiom;
+        public string RequiredArea { 
+            get
+            {
+                return $"Необходимая площадь - {_requiredarea}";
+            }
+            set
+            {
+                _requiredarea = value;
+            }
+        }
+        private string _requiredarea;
+        //public string[] TypeFood { 
+        //    get
+        //    {
+        //        return _typefood;
+        //    }
+        //    set
+        //    {
+        //        string[] _typefood = { value };
+        //    }
+        //}
+        //private string[] _typefood;
+        public string PredatorOrHerbivore { 
+            get
+            {
+                return $"{_predatororherbivore}";
+            }
+            set
+            {
+                if (value == "predator" ^ value == "herbivore")
+                {
+                    _predatororherbivore = value;
+                }
+            }
+        }
+        private string _predatororherbivore;
+        public string Sound { 
+            get
+            {
+                return _sound;
+            }
+            set
+            {
+                _sound = value;
+            }
+        }
+        private string _sound;
+
+        public string Name { get; set; }
+        public double FoodPerDay { get; set; }
+        public int Age { get; set; }
+
+        public void Eat()
+        {
+            eated += FoodPerDay;
+        }
+        public double eated = 0;
+        public void MakeSound()
+        {
+            Console.WriteLine(_sound);
+        }
+        public void Play()
+        {
+            Console.WriteLine($"{_kind} {Name} playing.");
+        }
+    }
+}
--- MyFirstProjectC/Program.cs
+++ MyFirstProjectC/Program.cs
@@ -504,7 +504,7 @@
 
 
 // 15th lesson
-using MyFirstProjectC;
+//using MyFirstProjectC;
 // 1
 //LinearEquation.SolveEquation();
 
@@ -540,5 +540,35 @@ using MyFirstProjectC;
 
 // 18th lesson
 //TwoDimensionalArrays.TestMinElement();
-TwoDimensionalArrays.TestMethods();
+//TwoDimensionalArrays.TestMethods();
+
+// 19th lesson
+using MyFirstProjectC;
+
+Animal animal1 = new Animal();
+animal1.Kind = "tiger";
+animal1.RequiredBiom = "savana";
+animal1.RequiredArea = "20 m^2";
+//animal1.TypeFood = { "fish", "meat" };
+animal1.PredatorOrHerbivore = "predator";
+animal1.Sound = "Rrrrrr";
+
+animal1.Name = "Simba";
+animal1.FoodPerDay = 7.5;
+animal1.Age = 9;
+
+
+Console.WriteLine(animal1.Kind);
+Console.WriteLine(animal1.RequiredBiom);
+Console.WriteLine(animal1.RequiredArea);
+Console.WriteLine(animal1.PredatorOrHerbivore);
+Console.WriteLine(animal1.Sound);
+Console.WriteLine(animal1.Name);
+for (int i = 0; i < 5; i++)
+{
+    animal1.Eat();
+    Console.WriteLine($"Всего съедено {animal1.eated}кг.");
+}
+Console.WriteLine($"Возраст {animal1.Age}");
+
 
